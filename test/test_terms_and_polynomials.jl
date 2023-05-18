432a232d708a2a136e937034daa36fef49cde5b7
Dominik Kuzinowicz <47429567+Pustey@users.noreply.github.com>
2023-05-18 17:34:13 +0200

    Update test_terms_and_polynomials.jl
    added test suites for functions:
    - compute_degree
    - get_unique_elements
    - deg_lex_sort

--- test/test_terms_and_polynomials.jl
+++ test/test_terms_and_polynomials.jl
@@ -2,6 +2,7 @@ using Test
 
 include("../src/terms_and_polynomials.jl")
 
+
 @testset "Test suite for purge" begin
     matrix_2 = [
         [1, 0, 1],
@@ -32,6 +33,9 @@ end
 
 
 @testset "Test suite for monomial_evaluation_set" begin
+    """
+    Tests whether monomial_evaluation_set works as intended.
+    """
     m1 = [4, 1, 2]
     X1 = [[1.0, 2.0, 3.0], [1.0, 0.5, 2.0]]
     coeff = 4.5
@@ -41,9 +45,65 @@ end
 
 
 @testset "Test suite for monomial_set_evaluation_set" begin
+    """
+    Tests whether monomial_set_evaluation_set works as intended.
+    """
     M1 = [[4, 1, 2], [3, 0, 2], [0, 2, 1]]
     X1 = [[1.0, 2.0, 3.0], [1.0, 0.5, 2.0]]
     coeffs = [2.0, 1.0, 5.0]
     @test monomial_set_evaluation_set(M1, X1) == [[18.0, 2.0], [9.0, 4.0], [12.0, 0.5]]
     @test monomial_set_evaluation_set(M1, X1, coeffs) == [[36.0, 4.0], [9.0, 4.0], [60.0, 2.5]]
+end
+
+
+matrix = [
+    [1,0,1],
+    [2,1,3],
+    [3,2,1],
+    [4,3,2],
+    [3,2,1]
+]
+matrix_sorted = [
+    [1,0,1],
+    [3,2,1],
+    [3,2,1],
+    [2,1,3],
+    [4,3,2]
+]
+matrix_sorted_unique = [
+    [1,0,1],
+    [3,2,1],
+    [2,1,3],
+    [4,3,2]
+]
+
+
+@testset "Test suite for get_unique_elements" begin
+    """
+    Tests whether get_unique_elements behaves as intended.
+    """
+    matrix_1, matrix_2 = copy(matrix), copy(matrix)
+    matrix_unique_1, matrix_unique_2, _ = get_unique_elements(matrix_1, matrix_2)
+    @test matrix_unique_1 == matrix_sorted_unique
+    @test matrix_unique_2 == matrix_sorted_unique
+end
+
+
+@testset "Test suite for compute_degree" begin
+    """
+    Tests whether compute_degree behaves as intended.
+    """
+    matrix_1 = copy(matrix)
+    @test compute_degree(matrix) == [2, 6, 6, 9, 6]
+end
+
+
+@testset "Test suite for deg_lex_sort" begin
+    """
+    Tests whether deg_lex_sort behaves as intended.
+    """
+    matrix_1, matrix_2 = copy(matrix), copy(matrix)
+    matrix_sorted_1, matrix_sorted_2, a = deg_lex_sort(matrix_1, matrix_2)
+    @test matrix_sorted_1 == matrix_sorted
+    @test matrix_sorted_2 == matrix_sorted
 end
