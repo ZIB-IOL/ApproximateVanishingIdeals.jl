aff768a41051e7864fe6bd151fed0a35f1ca2484
Dominik Kuzinowicz <47429567+Pustey@users.noreply.github.com>
2023-08-27 00:05:52 +0200

    Create test_oracle_avi.jl
    created tests for oracle_avi functions

---
+++ test/test_oracle_avi.jl
@@ -0,0 +1,32 @@
+using Test
+using Random
+
+include("oracle_avi.jl")
+include("auxiliary_functions.jl")
+include("terms_and_polynomials.jl")
+include("objective_functions.jl")
+
+
+@testset "Test suite for fit" begin
+  m, n = rand(15:25), rand(4:10)
+  X_train = rand(m, n)
+  X_train_transformed, sets = fit(X_train)
+  loss_list = Vector{Float64}([])
+  for col in 1:size(sets.G_evaluations, 2)
+    cur_col = sets.G_evaluations[:, col]
+    loss = 1 / size(cur_col, 1) * (norm(cur_col, 2)^2)
+    loss_list = append!(loss_list, loss)
+  end
+  @test all(loss_list .<= 0.1)
+end
+
+
+@testset "Test suite for l1_projection" begin
+  vec1 = rand(1:10, 20)
+  radius_1 = 2.5
+  radius_2 = 3.0
+  @test norm(l1_projection(vec1), 1) ≈ 1
+  @test norm(l1_projection(vec1, radius_1), 1) ≈ radius_1
+  @test norm(l1_projection(vec1, radius_2), 1) ≈ radius_2
+end
+  
