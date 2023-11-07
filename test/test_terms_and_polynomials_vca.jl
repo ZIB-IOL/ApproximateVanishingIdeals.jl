0f0c6a10a93997c6df29ec20f70856ed9f0c2a3a
Dominik Kuzinowicz <47429567+Pustey@users.noreply.github.com>
2023-11-07 12:47:25 +0100

    added tests
    added tests for vca.jl and terms_and_polynomials_vca.jl

---
+++ test/test_terms_and_polynomials_vca.jl
@@ -0,0 +1,85 @@
+include("auxiliary_functions.jl")
+include("terms_and_polynomials_vca.jl")
+
+using LinearAlgebra
+using Random
+using Test
+
+
+@testset "Testing suite for apply_V_transformation" begin
+    for i in 1:10
+        X_train = rand(i, 5)
+        X_train_transformed, sets_VCA = fit(X_train)
+        X_transformed, _ = apply_V_transformation(sets_VCA, X_train)
+        X_transformed = abs.(X_transformed)
+        @test all(X_train_transformed .- X_transformed .<= 1.0e-10)
+    end
+end;
+
+
+@testset "Testing suite for the SetsVCA functions construct_SetsVCA, update_F, F_to_matrix, update_V, V_to_matrix,
+update_C, construct_border, evaluate_transformation" begin
+    X = [[0.1; 0.2; 0.3];; [0.4; 0.5; 0.6];;]
+    
+    # test construct_SetsVCA
+    sets_VCA = construct_SetsVCA(X)
+    @test sets_VCA.X == X
+    @test sets_VCA.Cs == []
+    @test sets_VCA.Vs == []
+    @test sets_VCA.V_coefficient_vectors == []
+    @test sets_VCA.Fs[1] == 1/sqrt(size(X, 1)) * ones(Float64, size(X, 1), 1)
+    @test sets_VCA.F_coefficient_vectors == [ones(Float64, 1, 1)]
+    
+    # test construct_border
+    border = construct_border(sets_VCA)
+    @test border == X
+    
+    # test update_F
+    F_coeff = [[1.; 0.1];; [0.5; 0.2];;]
+    F_eval = [[0.5; 0.3; 0.4];; [2.; 1.; 1];;]
+    update_F(sets_VCA, F_coeff, F_eval)
+    @test sets_VCA.Fs[2] == F_eval
+    @test sets_VCA.F_coefficient_vectors[2] == F_coeff
+    
+    # test update_V
+    V_coeff = [[0.5;];;]
+    V_eval = [[0.1; 0.1; 0.2;];;]
+    update_V(sets_VCA, V_coeff, V_eval)
+    @test sets_VCA.Vs[1] == V_eval
+    @test sets_VCA.V_coefficient_vectors[1] == V_coeff
+    empty_vec = zeros(Float64, 0, 0)
+    update_V(sets_VCA, empty_vec, empty_vec)
+    @test sets_VCA.Vs[2] == empty_vec
+    @test sets_VCA.V_coefficient_vectors[2] == empty_vec
+    V_coeff = [[0.5; 0.2; 0.;];;]
+    V_eval = [[-0.3; 0.0; 0.2;];;]
+    update_V(sets_VCA, V_coeff, V_eval)
+    @test sets_VCA.Vs[3] == V_eval
+    @test sets_VCA.V_coefficient_vectors[3] == V_coeff
+    
+    # test update_C
+    C_evals = [[0.1; 0.2; 0.3];;]
+    update_C(sets_VCA, C_evals)
+    @test sets_VCA.Cs[1] == C_evals
+    
+    # test F_to_matrix
+    F_coeff = [[1.; -1.];;]
+    F_eval = [[-0.5; 0.3; 0.4];; [0.6; 0.9; 0.8];;]
+    update_F(sets_VCA, F_coeff, F_eval)
+    F_matrix = F_to_matrix(sets_VCA)
+    @test all(F_matrix .- [
+    [0.57735027; 0.57735027; 0.57735027];; [0.5; 0.3; 0.4];; [2; 1; 1];; [-0.5; 0.3; 0.4];; [0.6; 0.9; 0.8]] .<= 1.0e-10)
+    
+    # test V_to_matrix
+    V_matrix = V_to_matrix(sets_VCA)
+    @test V_matrix == [[0.1; 0.1; 0.2];; [-0.3; 0.; 0.2];;]  
+    
+    # test evaluate_transformation
+    (zeroes, entries, avg_sparsity, number_of_polynomials, number_of_terms, degree) = evaluate_transformation(sets_VCA)
+    @test zeroes == 1
+    @test entries == 4
+    @test avg_sparsity - 0.25 <= 1.0e-10
+    @test number_of_polynomials == 2
+    @test number_of_terms == size(F_to_matrix(sets_VCA), 2)
+    @test degree - 2.0 <= 1.0e-10
+end;
---
+++ test/test_vca.jl
@@ -0,0 +1,81 @@
+include("terms_and_polynoms_vca.jl");
+include("vca.jl");
+include("auxiliary_functions.jl");
+
+using LinearAlgebra
+using Random
+using Test
+
+
+@testset "Testing suite for VCA fit" begin
+    psi = 0.1
+    max_degree = 10
+    for i in 1:5
+        X_train = rand(i, 3)
+        X_train_transformed, sets_VCA = fit_vca(X_train; psi=psi, max_degree=max_degree)
+        for j in 1:size(X_train_transformed, 2)
+            @test 1/i * norm(X_train_transformed[:, j], 2)^2 <= psi
+        end
+    end
+end;
+
+
+@testset "Testing suite for find_range_null_vca" begin
+    for run in 1:10
+        F = rand(5, run)
+        C = rand(5, 2*run)
+        psi = 0.1
+        
+        V_coeffs, V_evals, F_coeffs, F_evals = find_range_null_vca(F, C, psi)
+        
+        for i in 1:size(F_coeffs, 2)
+            val = abs.(hcat(F, C) * F_coeffs[:, i] - F_evals[:, i])
+            @test all(val .<= 10e-10)
+            @test any(abs.(F_evals[:, i]) .> psi) 
+        end
+        
+        for i in 1:size(V_coeffs, 2)
+            val = abs.(hcat(F, C) * V_coeffs[:, i] - V_evals[:, i])
+            @test all(val .<= 10e-10)
+            @test all(1/5 .* norm(V_evals[:, i])^2 .<= psi)
+        end
+        
+    end
+end;
+
+
+@testset "Testing suite for evaluate_vca" begin 
+    m, n = rand(1:50), rand(1:50)
+    X_train = rand(m, n)
+    X_train_transformed, sets_train = fit_vca(X_train)
+    
+    if X_train_transformed != nothing
+        X_test_transformed, sets_test = evaluate_vca(sets_train, X_train)
+        @test X_test_transformed != nothing
+        @test all(X_train_transformed .- X_test_transformed .<= 1.0e-10)
+        
+        F_train_coeffs = sets_train.F_coefficient_vectors
+        F_test_coeffs = sets_test.F_coefficient_vectors
+        @test length(F_train_coeffs) == length(F_test_coeffs)
+        
+        for idx in 1:length(F_train_coeffs)
+            F_train_coeffs_cp = F_train_coeffs[idx]
+            F_test_coeffs_cp = F_test_coeffs[idx]
+            @test all(abs.(F_train_coeffs_cp .- F_test_coeffs_cp) .<= 1.0e-10)
+        end
+        
+        V_train_coeffs = sets_train.V_coefficient_vectors
+        V_test_coeffs = sets_test.V_coefficient_vectors
+        @test length(V_train_coeffs) == length(V_test_coeffs)
+        
+        for idx in 1:length(V_train_coeffs)
+            if size(V_train_coeffs[idx], 2) == 0
+                @test size(V_test_coeffs[idx], 2) == 0
+            else
+                V_train_coeffs_cp = V_train_coeffs[idx]
+                V_test_coeffs_cp = V_test_coeffs[idx]
+                @test all(abs.(V_train_coeffs_cp .- V_test_coeffs_cp) .<= 1.0e-10)
+            end
+        end
+    end
+end;
