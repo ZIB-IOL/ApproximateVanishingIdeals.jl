bca78dd36328051887f659dc51f2923bb31a6388
Dominik Kuzinowicz <47429567+Pustey@users.noreply.github.com>
2023-11-08 16:53:13 +0100

    Update test_terms_and_polynomials.jl
    shifted test for 'purge' to test_border_construction.jl

--- test/test_terms_and_polynomials.jl
+++ test/test_terms_and_polynomials.jl
@@ -4,37 +4,6 @@ include("../src/auxiliary_functions.jl")
 include("../src/terms_and_polynomials.jl")
 
 
-@testset "Test suite for purge" begin
-  matrix_2 = vecvec_to_mat([[1, 2, 3], 
-                            [0, 0, 1],
-                            [1, 1, 1]])
-    
-  matrix_1 = vecvec_to_mat([[1, 1],
-                            [0, 1],
-                            [0, 1]])
-    
-  matrix_2_purged, matrix_2_purged_2, _ = purge(matrix_2, 1. * matrix_2, matrix_1)
-    
-  @test size(matrix_2_purged, 2) == 0
-  @test size(matrix_2_purged_2, 2) == 0
-    
-  matrix_2 = vecvec_to_mat([[1, 2, 3],
-                            [0, 0, 1],
-                            [1, 1, 1]])
-    
-  matrix_1 = vecvec_to_mat([[1, 1],
-                            [0, 2],
-                            [2, 1]])
-    
-  matrix_2_purged, matrix_2_purged_2, _ = purge(matrix_2, 1. * matrix_2, matrix_1)
-    
-  @test matrix_2_purged == matrix_2_purged_2
-  @test matrix_2_purged == vecvec_to_mat([[1, 2, 3],
-                                          [0, 0, 1],
-                                          [1, 1, 1]])    
-end
-
-
 @testset "Test suite for update_coefficient_vectors" begin
   G_coefficient_vectors = reshape([1, 2, 0], 3, 1)
     
