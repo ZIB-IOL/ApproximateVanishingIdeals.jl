2515d837fec3405542446292db67021ada4e0fa5
Dominik Kuzinowicz <47429567+Pustey@users.noreply.github.com>
2023-11-08 17:04:57 +0100

    Create run_tests.jl

---
+++ test/run_tests.jl
@@ -0,0 +1,17 @@
+using Test
+
+println("running tests...")
+println("-----------------------------------------------------------")
+start_time = time()
+
+tests = filter(x -> startswith(x, "test_"), readdir("test"))
+@testset verbose = true "Test suite for AAPPR" begin
+    for test in tests
+        include(test)
+    end
+end
+elapsed_time = time() - start_time
+
+println("-----------------------------------------------------------")
+println("tests done")
+println("elapsed_time = $(round(elapsed_time, digits=3)) seconds")
