59ef17c6a2b244671a8d5ac6690e52fb7d209296
Dominik Kuzinowicz <47429567+dkuzi@users.noreply.github.com>
2024-02-06 13:41:43 +0100

    Update print_polynomials.jl

--- src/print_polynomials.jl
+++ src/print_polynomials.jl
@@ -4,7 +4,7 @@ Prints the polynomials obtained through OAVI as a LaTeX string.
 'digits' can be used to determine how many decimal places you want to round to. 
 Terms with rounded coefficient values 0.0 are omitted and coefficients with value 1.0 are omitted.
 """
-function print_polynomials(sets; digits::Int64=2, render=false)
+function print_polynomials(sets; digits::Int64=2)
     deg_ids = sets.O_degree_indices
 
     lt_idx_start = 1
